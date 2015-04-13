e5b3e8091b92b1d90a0302b82bbabcd93a1d18c0
qsgb <ron.liu.j@gmail.com>
2015-04-13 03:01:32 +0200

    plot3

---
+++ plot3.r
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\Jian\\Documents\\Study\\R book\\coursera R\\exdata_data_household_power_consumption")
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings = "?",colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+data2<-data[data$Date=="1/2/2007"|data$Date=="2/2/2007",]
+data2$Time<-paste(data2$Date,data2$Time)
+data2$Time<-strptime(data2$Time,"%d/%m/%Y %H:%M:%S")
+png("plot2.png",width = 6,height= 6,units="in",res=600)
+plot(data2$Time,data2$Sub_metering_1,xlab="",main="",ylab="Energy Sub metering ", type="n")
+lines(data2$Time,data2$Sub_metering_1, col="black")
+lines(data2$Time,data2$Sub_metering_2, col="red")
+lines(data2$Time,data2$Sub_metering_3, col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+dev.off()
